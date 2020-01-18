87e8b1558cfe59875f0a6d056a982502cc0e8e13
kumankoishi.py <7iggif@naver.com>
2020-01-18 23:19:54 +0900

    Modified Obstacles' Methods

--- Assets/ObstacleBehavior.cs
+++ Assets/ObstacleBehavior.cs
@@ -4,17 +4,23 @@ using UnityEngine;
 
 public class ObstacleBehavior : MonoBehaviour
 {
-    public static GameObject obj = GameObject.CreatePrimitive(PrimitiveType.Cube);
-
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.position = new Vector3(Random.Range(-4.5f, 4.5f), 0, transform.position.z);
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.position += new Vector3(0, 0, -0.05f);
+        if(transform.position.z < -10f)
+        {
+            // Reset Position
+            transform.position = new Vector3(Random.Range(-4.5f, 4.5f), 0, 10f);
+        }
+
+        // Box Hit
+        // ...
     }
 }
